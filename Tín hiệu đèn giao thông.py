16ee9dba7e1838cc4bac826ab853acdddccacd85
phandatt <phanbo756@gmail.com>
2025-11-16 10:19:31 +0700

    Tín hiệu đèn giao thông
    if-else

---
+++ Tín hiệu đèn giao thông.py
@@ -0,0 +1,15 @@
+tin_hieu = "xanh"
+print("Tín hiệu của đèn: ", tin_hieu)
+print("Hành động của xe: ")
+
+if tin_hieu == "xanh":
+    print("Xe được phép đi. ")
+elif tin_hieu == "vàng":
+    print("Xe đi chậm lại và chuẩn bị dừng. ")
+elif tin_hieu == "đỏ":
+    print("Xe dừng lại trước vạch. ")
+    
+else:
+    print("Tín hiệu lạ! Lỗi hệ thống. ")
+
+print("Chương trình kết thúc. ")
