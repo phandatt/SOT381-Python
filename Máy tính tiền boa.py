0b7eae3e2c0866b30edf5c6a21d82008cc3ce84d
phandatt <phanbo756@gmail.com>
2025-11-16 09:32:08 +0700

    Máy tính tiền boa
    Toán tử

---
+++ Máy tính tiền boa.py
@@ -0,0 +1,8 @@
+tong_hoa_don = 500000
+phan_tram_boa = 0.15
+tien_boa = tong_hoa_don * phan_tram_boa
+thanh_toan_cuoi_cung = tong_hoa_don + tien_boa
+print("Tổng hóa đơn là: ", tong_hoa_don)
+print("Phần trăm boa là: ", phan_tram_boa)
+print("Tiền boa là: ", tien_boa)
+print("Tổng thanh toán cuối cùng: ", thanh_toan_cuoi_cung)
