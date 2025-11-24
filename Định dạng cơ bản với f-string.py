f71b099a8d44c05ea0426aa436b7a29d7570b1cf
phandatt <phanbo756@gmail.com>
2025-11-24 07:45:25 +0700

    ĐỊnh dạng cơ bản với f-string
    f-string

---
+++ Định dạng cơ bản với f-string.py
@@ -0,0 +1,12 @@
+name = "Alice"
+age = 26
+score = 87.4763
+
+print(f"Tên: {name}, Tuổi: {age}, Điểm: {score}")
+
+print(f"Điểm làm tròn: {score:.2f}")
+print(f"Điểm phần trăm: {score:.1f}")
+
+print(f"Tên: {name:<10}")
+print(f"Tuổi: {age:>5}")
+print(f"Số nhị phân: {age:08b}")
