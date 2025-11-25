98875ec8c2e11f0af9f4ec9a97576bbae3ad0946
phandatt <phanbo756@gmail.com>
2025-11-25 09:27:37 +0700

    Chuyển đổi nhiệt độ
    f-string

--- Chuyển đổi nhiệt độ.py
+++ Chuyển đổi nhiệt độ.py
@@ -1,4 +1,20 @@
-do_C = 25
-do_F = 0
-do_F = do_C * 1.8 + 32
-print(do_C, "độ C tương đương với", do_F, "độ F")
+nhiet_do = float(input("Nhập nhiệt độ: "))
+loai_nhiet_do = input("Nhập loại nhiệt độ: ")
+
+if loai_nhiet_do == "C":
+    F = nhiet_do * 1.8 + 32
+    K = nhiet_do + 273.15
+    print(f"{F:.2f}")
+    print(f"{K:.2f}")
+elif loai_nhiet_do == "F":
+    C = (nhiet_do - 32) / 1.8
+    K = (nhiet_do + 459.67) / 1.8
+    print(f"{C:.2f}")
+    print(f"{K:.2f}")
+elif loai_nhiet_do == "K":
+    C = nhiet_do -273.15
+    F = (nhiet_do - 273.15) * 1.8 + 32
+    print(f"{C:.2f}")
+    print(f"{F:.2f}")
+else:
+    print("Loại không hợp lệ! Vui lòng nhập C, F hoặc K.")
