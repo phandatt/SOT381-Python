a4849640c9ece88e718a62223c9574da8ffcf7db
Phan Tấn Đạt (67130459) <phanbo756@gmail.com>
2025-12-15 14:04:02 +0700

    Update Số n.py

--- Số n.py
+++ Số n.py
@@ -3,4 +3,5 @@ n = int(input("Nhập số n: "))
 if n % 2 == 0 and n % 3 == 0:
     print("Đây là số chẵn chia hết cho 3. ")
 else:
-    print("Không phải là số chẵn chia hết cho 3. ")
+    print("Đây là số chẵn nhưng không chia hết cho 3. ")
+
