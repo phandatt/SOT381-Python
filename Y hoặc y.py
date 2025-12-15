82552cb05289d4c9114cbf995e80dd8846553664
Phan Tấn Đạt (67130459) <phanbo756@gmail.com>
2025-12-15 15:09:40 +0700

    Add files via upload

---
+++ Y hoặc y.py
@@ -0,0 +1,5 @@
+while True:
+    user = input("Nhập Y hoặc y:")
+    if user == "Y" or user == "y":
+        print("Login success")
+    break
---
+++ giai thừa.py
@@ -0,0 +1,7 @@
+n = int(input("Nhập số n: "))
+
+giai_thua = 1
+for i in range (1, n + 1):
+    giai_thua *= i
+
+print(giai_thua)
