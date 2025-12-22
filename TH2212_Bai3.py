902ba30a057443671828489103b1037c071c64dc
Phan Tấn Đạt (67130459) <phanbo756@gmail.com>
2025-12-22 15:18:01 +0700

    Add files via upload

---
+++ TH2212_Bai3.py
@@ -0,0 +1,9 @@
+def so_lon_nhat(a, b, c):
+    return max(a, b, c)
+
+a = float(input("Nhập số a: "))
+b = float(input("Nhập số b: "))
+c = float(input("Nhập số c: "))
+
+print("Số lớn nhất là: ", so_lon_nhat(a, b, c))
+
