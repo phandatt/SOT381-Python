e9671dbba50a369415a82402d3cd453a3e06411f
Phan Tấn Đạt (67130459) <phanbo756@gmail.com>
2025-12-22 14:25:23 +0700

    Update TH2212_Bai1.py

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -3,8 +3,11 @@ while True:
     h = float(input("Nhập cạnh h: "))
     if 0.0 <= w and h <= 100.0:
         break
+    else:
+        print("Sai! Nhập lại")
 chu_vi = (w + h) * 2
 dien_tich = w * h
 
 print(f"Chu vi hình chữ nhật là: {chu_vi:.2f}")
 print(f"Diện tích hình chữ nhật là: {dien_tich:.2f}")
+
