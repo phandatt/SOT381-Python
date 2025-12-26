d531f49a18e273c7bde20ceb316593912ec40823
phandatt <phanbo756@gmail.com>
2025-12-26 14:57:19 +0700

    Create Tính thương 2 số.py

---
+++ Tính thương 2 số.py
@@ -0,0 +1,10 @@
+# Tính thương hai số: Nhập hai số và in ra thương của chúng.
+
+a = float(input("Nhập a: "))
+b = float(input("Nhập b: "))
+
+if b != 0:
+    T = a / b
+    print("Thương của hai số là:", T)
+else:
+    print("Không thể chia cho 0.")
