c3700d671375b0a22ecdefb82262fe2725b98a1d
phandatt <phanbo756@gmail.com>
2025-12-27 15:33:24 +0700

    Create Tính bình phương 1 số.py

---
+++ Bai_Tu_Hoc/Tính bình phương 1 số.py
@@ -0,0 +1,10 @@
+# Tính bình phương của một số: Nhập một số và in ra bình phương của nó.
+
+# Nhập một số từ bàn phím
+a = float(input("Nhập a: "))
+
+# Tính bình phương của số
+binh_phuong = a ** 2
+
+# In kết quả
+print("Bình phương của số là:", binh_phuong)
